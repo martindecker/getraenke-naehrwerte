a97f1c9ce67a0b581ebab7cefa1478d7879ca396
martindecker <c-m-2005@onlinehome.de>
2020-10-30 00:00:05 +0100

    Prepare List of drinks

--- scripts4/getraenke-naehrwerte.py
+++ scripts4/getraenke-naehrwerte.py
@@ -1,7 +1,10 @@
 import kivy 
 from kivy.app import App 
 from kivy.uix.label import Label 
-  
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+from kivy.uix.listview import ListItemButton
+   
 kivy.require('1.11.1')   
   
 class MyFirstKivyApp(App): 
