59b667063ded6ec69f9b5708d60267b08ab8401c
martindecker <c-m-2005@onlinehome.de>
2020-10-31 23:59:55 +0100

    Prepare List 2

--- getraenke-naehrwerte.py
+++ getraenke-naehrwerte.py
@@ -1,15 +1,15 @@
 import kivy 
 from kivy.app import App 
 from kivy.uix.label import Label 
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
-from kivy.uix.listview import ListItemButton
+from kivy.lang import Builder
+from kivy.uix.recycleview import RecycleView
+
    
 kivy.require('1.11.1')   
   
 class MyFirstKivyApp(App): 
      def build(self): 
-        return Label(text ="Program is still empty.")           
+        return Label(text ="Still no List displayed.")           
   
 print("begin gui")
 MyFirstKivyApp().run()  
